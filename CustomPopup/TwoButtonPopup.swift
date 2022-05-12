78737b9f2369996196bdfb897bb6b2b78a3afc2f
junyoung <junlight94@gmail.com>
2022-05-12 19:51:32 +0900

    완료

--- CustomPopup/TwoButtonPopup.swift
+++ CustomPopup/TwoButtonPopup.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
-class TwoButtonPopup: UIViewController {
+// delegate protocol
+protocol DisDelegate{
+    func delegateString(text: String)
+}
 
+class TwoButtonPopup: UIViewController {
+    
+    var delegate: DisDelegate?
+    
+    //main에서 받아온 Label
+    @IBOutlet var presentLabel: UILabel!
+    @IBOutlet var popupView: UIView!
+    
+    //main에서 받을 String 형 옵셔널 변수
+    var receiveString: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        //팝업뷰의 cornerRadius
+        popupView.layer.cornerRadius = 6
+        
+        //main에서 받아온 text Label에 표시
+        presentLabel.text = receiveString
     }
-
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
+    }
+    
+    @IBAction func confirmButtonAction(_ sender: Any) {
+        dismiss(animated: false) {
+            self.delegate?.delegateString(text: "확인 버튼을 눌렀습니다.")
+        }
+    }
 }
--- CustomPopup/ViewController.swift
+++ CustomPopup/ViewController.swift
@@ -7,7 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, DisDelegate {
+    //delegate required
+    func delegateString(text: String) {
+        //팝업에서 delegate한 값 textLabel에 뿌려주기
+        print(text)
+        textLabel.text = text
+    }
+    
 
     //팝업 문구 가져올 라벨
     @IBOutlet var textLabel: UILabel!
@@ -15,13 +22,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("시작")
+        // textField delegate
+        textField.delegate = self
     }
-
+    // 화면 클릭 키보드 내리기
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     // 팝업 띄울 버튼 Action
     @IBAction func popupButtonAction(_ sender: Any) {
-        
+        let popup = TwoButtonPopup(nibName: "TwoButtonPopup", bundle: nil)
+        popup.modalPresentationStyle = .overCurrentContext
+        popup.delegate = self
+        popup.receiveString = textField.text
+        self.present(popup, animated: false)
     }
     
 }
